6ea91a780043a4cd8d0764db501211413021e3ad
nmn <nmngarg030798@gmail.com>
2021-12-18 21:59:51 +0530

    adddddd

--- block-BJaacf/code/index.js
+++ block-BJaacf/code/index.js
@@ -1,30 +1,79 @@
 // 1. Create an array named numbers and store 5 number values in it
 
 let numbers = [
-    1,2,3,4,5
+    11, 22, 33, 44, 55
 ]
 
 // 2. Calculate the sum of array items and print it to the console using console.log()
 
-for(let i = 0; i < numbers.length; i++){
-    // numbers[i] += i
+let sum = 0;
+for(let i=0; i < numbers.length; i++){
+    sum += numbers[i]
 }
-// console.log(numbers[i]);
+
+console.log(sum);
+
+let forsum = 0;
+for(let number of numbers){
+    forsum += number;
+}
+
+console.log(forsum);
 
 // 3. Calculate the average of array items and print it to the console using console.log()
 
-
+console.log(sum/numbers.length);
 
 // 4. Find the highest number in the array and print it to the console using console.log()
 
+let highestNumber = numbers[0];
+for(let number of  numbers){
+    if(number>highestNumber){
+        highestNumber = number
+    }
+} 
+
+console.log(highestNumber);
+
 // 5. Find the lowest number in the array and print it to the console using console.log()
+
+let lowestNumber = numbers[0];
+for(let number of  numbers){
+    if(number<lowestNumber){
+        lowestNumber = number
+    }
+} 
+
+console.log(lowestNumber);
 
 // 6. Find the even numbers in the array and print them to the console using console.log()
 
+for(let number of numbers){
+    if(number % 2 === 0){
+        console.log(number, "even");
+    }
+}
+
 // 7. Find the odd numbers in the array and print them to the console using console.log()
 
+for(let number of numbers){
+    if(number % 2 !== 0){
+        console.log(number, "odd");
+    }
+}
 // 8. Find the numbers in the array that is divisible by 5 and print them to the console using console.log()
-
+for(let number of numbers){
+    if(number % 5 === 0){
+        console.log(number, "divisible by 5");
+    }
+}
 // 9. Log all the element of the array one by one
-
+for(let number of numbers){
+    console.log(number);
+}
 // 10. Find all the number in the array that is divisible by 3
+for(let number of numbers){
+    if(number % 3 === 0){
+        console.log(number, "divisible by 3");
+    }
+}
