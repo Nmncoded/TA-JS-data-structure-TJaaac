d97b3c3a913df5b1ce6f6fa0dbb5306ef5586128
nmn <nmngarg030798@gmail.com>
2021-12-21 00:02:44 +0530

    ad

--- block-BJaacr/code/index.js
+++ block-BJaacr/code/index.js
@@ -20,11 +20,19 @@ Hint: (Structure of got variable)
 
 // 1. Log the value of key "houses"
 
-// 2. Log the length of the "houses" key
+console.log(got.houses);
 
+// 2. Log the length of the "houses" key
+console.log(got.houses.length);
 // 3. Log the name of all houses in got one by one
 
+for(let house of got.houses){
+  console.log(house);
+}
 // 4. Log the name of all the people in house Starks (index 0) one by one
+// for(let i=0; i < got.houses[0].people.length-1; i++ ){
+  // console.log(got.houses[0].people[i].name)
+}
 
 // 5. Log the name of all the people in house Lannisters (index 1) one by one
 
